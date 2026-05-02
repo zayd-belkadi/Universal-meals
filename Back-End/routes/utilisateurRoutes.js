ec73c5dd5d76c611d98b77fba1865656724ac239
zayd_belkadi <zaydbelkadi06@gmail.com>
2026-05-02 02:50:16 -0400

    Fix : Finalisation cahier des charges (sécurité catégories, middleware employé, liste utilisateurs)

--- Back-End/routes/utilisateurRoutes.js
+++ Back-End/routes/utilisateurRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const utilisateurController = require('../controllers/utilisateurController');
+const auth = require('../middlewares/authMiddleware');
 
 // Route pour l'inscription (POST /api/utilisateurs/inscription)
 router.post('/inscription', utilisateurController.inscription);
 
 // Route pour la connexion (POST /api/utilisateurs/connexion)
 router.post('/connexion', utilisateurController.connexion);
+
+//
+router.get('/', auth.verifierToken, auth.estAdmin, utilisateurController.getAllUtilisateurs);
 
 module.exports = router;
